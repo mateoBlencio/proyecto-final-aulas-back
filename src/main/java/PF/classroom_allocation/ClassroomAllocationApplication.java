533c63a927e0274d4db20b817acf48e090491f6b
Matias Gil <ezemgil@gmail.com>
2026-05-28 13:18:58 -0300

    Commit inicial

---
+++ src/main/java/PF/classroom_allocation/ClassroomAllocationApplication.java
@@ -0,0 +1,13 @@
+package PF.asignacion_aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsignacionAulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsignacionAulasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PF/classroom_allocation/ClassroomAllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package PF.classroom_allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsignacionAulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
