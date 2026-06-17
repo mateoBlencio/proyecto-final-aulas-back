d7628b77165a66741f17cfe3277e707a9f4cd176
mateoBlencio <mateoblencio@gmail.com>
2026-06-17 16:10:52 -0300

    refactorización(backend): mover GlobalExceptionHandler a common y crear excepción base

--- src/main/java/ar/edu/utn/frc/classroom_allocation/space/exception/SpaceDomainException.java
+++ src/main/java/ar/edu/utn/frc/classroom_allocation/space/exception/SpaceDomainException.java
@@ -1,8 +1,11 @@
 package ar.edu.utn.frc.classroom_allocation.space.exception;
 
-public class SpaceDomainException extends RuntimeException {
+import ar.edu.utn.frc.classroom_allocation.common.exception.ClassroomAllocationAppException;
+import org.springframework.http.HttpStatus;
+
+public class SpaceDomainException extends ClassroomAllocationAppException {
 
     public SpaceDomainException(String message) {
-        super(message);
+        super(HttpStatus.BAD_REQUEST, "Space domain error", message);
     }
 }
