75f3886079e60c4ed734af41aec3b53e943368d1
mateoBlencio <mateoblencio@gmail.com>
2026-06-20 13:56:03 -0300

    nueva-funcionalidad(materias): implementar entidades Specialty, StudyPlan, Subject y sus repositorios

---
+++ src/main/java/ar/edu/utn/frc/classroom_allocation/career/repository/SubjectRepository.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.classroom_allocation.career.repository;
+
+import ar.edu.utn.frc.classroom_allocation.career.model.StudyPlan;
+import ar.edu.utn.frc.classroom_allocation.career.model.Subject;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
+    Optional<Subject> findByCodigoMateriaAndPlanAndDeletedFalse(Integer codigoMateria, StudyPlan plan);
+}
