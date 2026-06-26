5282e54c1d8a9cefd138f3d163ba239b2206a442
mateoBlencio <mateoblencio@gmail.com>
2026-06-26 10:55:29 -0300

    configuración(backend): data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO tipo_aula (descripcion, eliminado)
-SELECT 'Aula Standard', false
-WHERE NOT EXISTS (SELECT 1 FROM tipo_aula WHERE descripcion = 'Aula Standard');
+SELECT 'Aula', false
+WHERE NOT EXISTS (SELECT 1 FROM tipo_aula WHERE descripcion = 'Aula Normal');
