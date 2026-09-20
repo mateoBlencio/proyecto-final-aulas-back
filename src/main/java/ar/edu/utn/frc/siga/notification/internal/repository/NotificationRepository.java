8d30989a0d933abecdf487eced4553a2d75b5ff5
Matías Gil <ezemgil@gmail.com>
2026-09-20 12:31:21 -0300

    feat(notification): selección de canales activos y encolado outbox

--- src/main/java/ar/edu/utn/frc/siga/notification/internal/repository/NotificationRepository.java
+++ src/main/java/ar/edu/utn/frc/siga/notification/internal/repository/NotificationRepository.java
@@ -1,7 +1,11 @@
 package ar.edu.utn.frc.siga.notification.internal.repository;
 
+import ar.edu.utn.frc.siga.notification.api.NotificationChannel;
 import ar.edu.utn.frc.siga.notification.internal.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+    boolean existsByIdempotencyKeyAndChannelAndRecipient(
+            String idempotencyKey, NotificationChannel channel, String recipient);
 }
