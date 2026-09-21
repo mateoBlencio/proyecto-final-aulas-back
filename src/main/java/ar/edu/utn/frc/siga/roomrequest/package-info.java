eb6ac8806f883be9c20d7ab9bdfff6a835b13dc8
Matías Gil <ezemgil@gmail.com>
2026-09-21 14:12:44 -0300

    feat(roomrequest): manda el mail al docente al notificar un pedido resuelto

--- src/main/java/ar/edu/utn/frc/siga/roomrequest/package-info.java
+++ src/main/java/ar/edu/utn/frc/siga/roomrequest/package-info.java
@@ -1,4 +1,5 @@
-@ApplicationModule(allowedDependencies = { "academic :: api", "space :: api", "events :: api", "allocation :: api", "audit :: api", "auth :: api", "common" })
+@ApplicationModule(allowedDependencies = { "academic :: api", "space :: api", "events :: api", "allocation :: api",
+        "audit :: api", "auth :: api", "notification :: api", "common" })
 package ar.edu.utn.frc.siga.roomrequest;
 
 import org.springframework.modulith.ApplicationModule;
